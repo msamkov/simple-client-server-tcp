254a42b77389e896774ca75f33899716673e7c27
Max Samkov <littlebiggame2@gmail.com>
2018-02-07 10:43:56 +0300

    Простой клиент и сервер tcp

---
+++ tcp_server.py
@@ -0,0 +1,24 @@
+import socket
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host, port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection from: " + str(addr))
+    while True:
+        data = c.recv(1024)
+        data = data.decode('utf-8')
+        if not data:
+            break
+        print("from connected user: " + data)
+        data = str(data).upper()
+        print("sending: " + data)
+        c.send(data.encode('utf-8'))
+    c.close()
+
+if __name__ == '__main__':
+    Main()
